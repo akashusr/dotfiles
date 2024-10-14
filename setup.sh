fb91febab10f910950c065a4a7f979f4921c5d25
Akash Ahmed <71524256+akashusr@users.noreply.github.com>
2024-10-15 02:45:50 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# Add this to your dotfiles repo
+if [ ! -f .gitpod.yml ]; then
+    cp ~/dotfiles/.gitpod.yml .gitpod.yml  # Assuming you store the default .gitpod.yml in the dotfiles repository
+fi
